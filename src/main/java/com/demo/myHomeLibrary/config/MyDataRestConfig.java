3d957afcf6921f9a326e3cbb56eed0be6c524e56
Angjela Mijakovska <angjela.mijakovska@gmail.com>
2023-10-04 23:04:34 +0200

    REST APIs: disable PUT, POST, DELETE (configure for read-only)

---
+++ src/main/java/com/demo/myHomeLibrary/config/MyDataRestConfig.java
@@ -0,0 +1,30 @@
+package com.demo.myHomeLibrary.config;
+
+import com.demo.myHomeLibrary.entity.Book;
+import com.demo.myHomeLibrary.entity.BookCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+        HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        config.getExposureConfiguration()
+                .forDomainType(Book.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)));
+
+        config.getExposureConfiguration()
+                .forDomainType(BookCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)));
+
+    }
+}
