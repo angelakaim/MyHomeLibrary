a86af5f86c5a4a173260caa02c2df6e6d85adc70
Angjela Mijakovska <angjela.mijakovska@gmail.com>
2023-10-05 15:35:01 +0200

    Minor fix (rename product-list component to book-list)

--- myHomeLibraryFrontend/angular-homelibrary/src/app/app.module.ts
+++ myHomeLibraryFrontend/angular-homelibrary/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { BookListComponent } from './components/book-list/book-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
